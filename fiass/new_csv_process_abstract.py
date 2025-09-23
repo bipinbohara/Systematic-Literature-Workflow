032cd8686f36529ec95290a9b9cae3bac0101c9a
Bipin Bohara <bipin.bohara20@gmail.com>
2025-09-23 16:32:03 -0500

    Update new_csv_process_abstract.py

--- fiass/new_csv_process_abstract.py
+++ fiass/new_csv_process_abstract.py
@@ -24,8 +24,8 @@ OUTPUT_CSV.parent.mkdir(parents=True, exist_ok=True)
 # ---------- LLM config ----------
 # Prefer setting these via env if you change endpoints/models later.
 #LLM_URL     = os.environ.get("LLM_URL", "http://192.168.0.205:80/v1/completions")
-LLM_URL     = os.environ.get("LLM_URL", "http://192.168.0.213:80/v1/completions")
-LLM_MODEL   = os.environ.get("LLM_MODEL", "openai/gpt-oss-120b")
+LLM_URL     = os.environ.get("LLM_URL", "http://192.168.0.213:8080/v1/completions")
+LLM_MODEL   = os.environ.get("LLM_MODEL", "Qwen/Qwen3-235B-A22B-Instruct-2507-FP8")
 LLM_API_KEY = os.environ.get("LLM_API_KEY")  # optional
 TIMEOUT     = int(os.environ.get("LLM_TIMEOUT", "600"))
 
